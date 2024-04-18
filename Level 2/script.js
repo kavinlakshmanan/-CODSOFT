b9a84632c5b9d8c79496bd29fb7d2f383a2d9c44
Kavin L <142073658+kavinlakshmanan@users.noreply.github.com>
2024-04-18 20:49:43 +0530

    Delete Level 2 directory

--- Level 2/script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-function openInNewTab(url) {
-    window.open(url, '_blank');
-  }
